c35ef3d4b6a5001d4e27ba936900d62301b8c997
Maryam <mariamsaeedalshamy@gmail.com>
2026-09-01 12:49:00 +0300

    QUestion 1:

--- csharp_basics_ass1/Program.cs
+++ csharp_basics_ass1/Program.cs
@@ -2,9 +2,23 @@
 {
     internal class Program
     {
+        internal class Book
+        {
+            public string title;
+            public int pages;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Question 1:
+            Book b1 = new Book()
+            {
+                title = "Atomic Habits",
+                pages = 430
+            };
+            object b2 = b1;
+            Console.WriteLine(b2);
+
+            // -----------------------
+            }
         }
     }
-}
