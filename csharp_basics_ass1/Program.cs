135fc6fc53f542eb04179dafaf7a1fd4be0da37d
maria <maria@MAYAM>
2026-09-01 12:03:45 +0300

    Add project files.

---
+++ csharp_basics_ass1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace csharp_basics_ass1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
